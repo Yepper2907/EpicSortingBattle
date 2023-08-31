eeaad3985a315d880bb5122563728fc6f96e5208
Goup project <matt1207@zbc.dk>
2023-08-31 15:43:06 +0200

    bubble sore done, probaly

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,7 +1,18 @@
 import random, copy
 
-def bubblesort (items):
- n = items
+def bubbleSort (items):
+    n = len(items)
+    swapped = False
+    for i in range(n-1):
+        for j in range(0,n-i-1):
+            if items[j]> items[j+1]:
+                swapped = True
+                items[j], items[j+1] = items[i+1], items[i]
+
+            if not swapped:
+                break
+    return items
+
 
 
 if __name__ == '__main__':
@@ -10,11 +21,10 @@ if __name__ == '__main__':
     for i in range(50):
         random.shuffle(lb)
         ## Kald den funktion, du vil teste
-        ls = bogoSort(l)
+        ls = bubbleSort(l)
         ## Kald den funktion, du vil teste
         if ls != l:
             print('Fejl! Algoritmen kan ikke sortere.')
             break
-    print('Succes! Algoritmen sorterer korrekt.')
     print('blandet: ', lb)
     print('sorteret:', ls)

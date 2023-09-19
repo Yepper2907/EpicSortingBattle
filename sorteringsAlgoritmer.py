1a29f91d534a0b5a622fa8a6c11c5b2120da6164
Yepper2907 <emil.capion@gmail.com>
2023-09-19 08:48:27 +0200

    Tilføjet selection sort

--- sorteringsAlgoritmer.py
+++ sorteringsAlgoritmer.py
@@ -1,19 +1,29 @@
 import random, copy
 
-def bubbleSort (items):
-    n = len(items)
-    swapped = False
-    for i in range(n-1):
-        for j in range(0,n-i-1):
-            if items[j]> items[j+1]:
-                swapped = True
-                items[j], items[j+1] = items[i+1], items[i]
+def selectionSort(arr):
+    arr=arr.copy()
+
+
+def bubbleSort(arr):
+        arr=arr.copy()
+        n = len(arr)
+
+        swapped = False
+
+        for i in range(n - 1):
+
+            for j in range(0, n - i - 1):
+
+
+                if arr[j] > arr[j + 1]:
+                    swapped = True
+                    arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
 
             if not swapped:
                 break
-    return items
 
-
+        return arr
 
 if __name__ == '__main__':
     l = list(range(0, 5))
@@ -21,10 +31,11 @@ if __name__ == '__main__':
     for i in range(50):
         random.shuffle(lb)
         ## Kald den funktion, du vil teste
-        ls = bubbleSort(l)
+        ls = bubbleSort(lb)
         ## Kald den funktion, du vil teste
         if ls != l:
             print('Fejl! Algoritmen kan ikke sortere.')
             break
+    print('Succes! Algoritmen sorterer korrekt.')
     print('blandet: ', lb)
     print('sorteret:', ls)
